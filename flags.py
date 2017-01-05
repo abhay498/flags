90be7ec8b800dbbc982cf4030100f22187cb58b3
Abhay Kumar Singh <abskumar798@gmail.com>
2017-01-05 19:39:41 +0530

    added icon for about window

--- flags.py
+++ flags.py
@@ -33,14 +33,14 @@ def about():
     window = Toplevel()
     window.title("About flags")
     window.geometry("400x400")
-
+    window.iconbitmap('C:\\Users\\abhayksi\\Desktop\\GUI_python\\favicon.ico')
     canvas = Canvas(window,width = 400,height = 400,bg = 'white')
     canvas.pack()
     J_one = canvas.create_text(150,150, text=("File version : 2.1\rEmail : abskumar798@gmail.com"), font=("Comic Sans", 10))
     exit_button = Button(canvas,width = 20, text = "Close", fg = "black",\
                              command = lambda:app.exit_window(window))
     exit_button.place(relx=.78, rely=.93, anchor="c")
-    
+
 class game_wrapper(Tk):
     def __init__(self,*args,**kwargs):
         Tk.__init__(self,*args,**kwargs)
