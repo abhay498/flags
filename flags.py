0f46b7a8ab22b14df593cbe6b98b848a0530fddf
Abhay Kumar Singh <abskumar798@gmail.com>
2022-06-01 12:13:53 +0530

    Corrected flags page

--- flags.py
+++ flags.py
@@ -10,7 +10,7 @@ global shuffled_country
 import copy
 from random import shuffle
 counter = 0
-country = ['Canada','Australia','Belarus','Croatia','Estonia','Ethiopia']
+country = ['Canada','Australia']
 random.shuffle(country)
 #print(country)
 import pdb
@@ -18,9 +18,10 @@ global chosen
 global button1,button2,button3,button4
 global top_frame,bottom_frame
 global flag
+from time import sleep
 
 def func_options(chosen):
-    country_1 = ['Canada','Australia','Belarus','Croatia','Estonia','Ethiopia']
+    country_1 = ['Canada','Australia','Croatia','Estonia','Ethiopia']
     country_2 = copy.copy(country_1)
     country_2.remove(chosen)
     options = []
@@ -34,11 +35,11 @@ def func_options(chosen):
 
     shuffle(options)
     del country_2[:]
-    return(options)
+    return options
     
 def change_flag(top_frame,bottom_frame,button1,button2,button3,button4):
-    global counter,canvas,my_image,chosen,flag
-    directory = 'C:\\Users\\abhayksi\\Desktop\\GUI_python\\'
+    global counter,canvas,my_image,chosen,flag, directory
+    directory = 'national_flags\\'
     format_image = '.png'
     location = directory + country[counter] + format_image
     my_image = PhotoImage(file = location)
@@ -49,7 +50,6 @@ def change_flag(top_frame,bottom_frame,button1,button2,button3,button4):
     for i in range(0,4):
         if chosen == options_text[i]:
             flag = i
-            #print(flag + 1)
             break
     flag = flag + 1
     button1["text"] = options_text[0]
@@ -61,32 +61,27 @@ def change_flag(top_frame,bottom_frame,button1,button2,button3,button4):
     
 def print_result(number,id_2):
     global userscore_canvas,chosen,flag
-    print(flag)
-    print(number)
     if flag == number:
         userscore_canvas.itemconfigure(id_2, text = "Right")
     else:
         userscore_canvas.itemconfigure(id_2, text = "Wrong")
 
 def user_name():
-    print ("Hey {0},lets get started".format(svalue.get()))
+    print ("Have fun.".format(svalue.get()))
 
 def about():
     window = Toplevel()
     window.title("About flags")
     window.geometry("400x400")
-    window.iconbitmap('C:\\Users\\abhayksi\\Desktop\\GUI_python\\favicon.ico')
+    window.iconbitmap('favicon.ico')
     canvas = Canvas(window,width = 400,height = 400,bg = 'white')
     canvas.pack()
-    J_one = canvas.create_text(150,150, text=("File version : 1.0\rEmail : abskumar798@gmail.com"), font=("Comic Sans", 10))
-    exit_button = Button(canvas,width = 20, text = "Close", fg = "black",\
-                             command = lambda:app.exit_window(window))
-    exit_button.place(relx=.78, rely=.93, anchor="c")
-
+    J_one = canvas.create_text(150,150, text=("File version : 1.0\r\rEmail : abskumar798@gmail.com"), font=("Comic Sans", 10))
+        
 class game_wrapper(Tk):
     def __init__(self,*args,**kwargs):
         Tk.__init__(self,*args,**kwargs)
-        self.geometry('500x650')
+        self.geometry('500x550')
         self.title("flags")
         self.frames = {}
         self.configure(bg="white")
@@ -118,10 +113,14 @@ class flags_page(Frame):
         Frame.__init__(self,parent)
         global canvas,score,userscore_canvas,counter,country,shuffled_country
         score = 870
-        self.configure(bg="white")
         canvas = Canvas(self,width = 250,height = 130,bg = 'white')
         canvas.pack()
         canvas.place(relx=0.25, rely= 0.3)
+        
+        filename = PhotoImage(file = "national_flags\\Australia.png")
+        Frame.filename = filename
+        image_container = canvas.create_image(5,5,anchor = NW,image = filename)
+        
         next_frame = Frame(self,bg= "blue")
         next_frame.pack(side = "top",fill = X)
         top_frame = Frame(self,bg = "white")
@@ -136,14 +135,12 @@ class flags_page(Frame):
         id_1 = userscore_canvas.create_text(40,30, text=("Score", score), font=("Comic Sans",10))
         id_2 = userscore_canvas.create_text(45,45, font=("Comic Sans",10))
 
-        #options_text = ['Aus','Canada','Belgium','Russia']
-        #options_text = func_options()
-        #print(options_text)
-        #pdb.set_trace()
-        button1 = Button(top_frame,width = 20, text = "a", fg = "black",command = lambda: print_result(1,id_2))
-        button2 = Button(top_frame,width = 20, text = "b", fg = "black",command = lambda: print_result(2,id_2))
-        button3 = Button(bottom_frame,width = 20, text = "c", fg = "black",command = lambda: print_result(3,id_2))
-        button4 = Button(bottom_frame,width = 20, text = "d", fg = "black",command = lambda: print_result(4,id_2))
+        ## Set the options here
+        options = options_text = func_options('Australia')
+        button1 = Button(top_frame,width = 20, text = options[0], fg = "black",command = lambda: print_result(1,id_2))
+        button2 = Button(top_frame,width = 20, text = options[1], fg = "black",command = lambda: print_result(2,id_2))
+        button3 = Button(bottom_frame,width = 20, text = options[2], fg = "black",command = lambda: print_result(3,id_2))
+        button4 = Button(bottom_frame,width = 20, text = options[3], fg = "black",command = lambda: print_result(4,id_2))
         button1.pack(side = LEFT,padx = 5,pady = 5)
         button2.pack(side = RIGHT,padx = 5,pady = 5)
         button3.pack(side = LEFT,padx = 5,pady = 5)
@@ -159,24 +156,25 @@ class flags_page(Frame):
 
         menubar = Menu(self)
         
-        #****** file 
+        ## File 
         filemenu = Menu(menubar,tearoff=0)
         filemenu.add_command(label="Start",command = lambda:controller.show_frame(start_page))
         filemenu.add_command(label="Play",command = lambda:controller.show_frame(flags_page))
         filemenu.add_command(label="Exit",command = lambda:app.exit())
         menubar.add_cascade(label="File",menu = filemenu)
 
-        #****** options
+        ## Options
         options = Menu(menubar,tearoff=0)
         options.add_command(label="Scores",command = lambda:controller.show_frame(score_page))
         menubar.add_cascade(label="Options",menu = options)
 
-        #****** help
-        helps = Menu(menubar,tearoff=0)
-        helps.add_command(label="About",command = lambda: about())
-        menubar.add_cascade(label="Help",menu = helps)
+        ## Help
+        help = Menu(menubar,tearoff=0)
+        help.add_command(label="About",command = lambda: about())
+        menubar.add_cascade(label="Help",menu = help)
         
         controller.configure(menu = menubar)
+
 
 class start_page(Frame):
     def __init__(self,parent,controller):
@@ -185,7 +183,7 @@ class start_page(Frame):
         global background_label
         global svalue
 
-        background_image = PhotoImage(file = 'C:\\Users\\abhayksi\\Desktop\\GUI_python\\white.png')
+        background_image = PhotoImage(file = 'white.png')
         background_label = Label(self, image=background_image)
         background_label.pack(side='top', fill='both', expand='yes')
         label = Label(background_label,text = "Start page",font = LARGE_FONT)
@@ -198,7 +196,7 @@ class start_page(Frame):
         w = Entry(self,textvariable=svalue)
         w.pack()
         
-        foo = Button(self,text="Username", command = lambda: user_name())
+        foo = Button(self,text="Name", command = lambda: user_name())
         foo.pack()
         
 class score_page(Frame):
@@ -208,17 +206,17 @@ class score_page(Frame):
         user_one = 46
         score_canvas= Canvas(self,width=500, height=700, bg="white")
         score_canvas.pack()
-        J_one = score_canvas.create_text(200,100, text=("python", user_one), font=("Comic Sans", 20))
-        J_two = score_canvas.create_text(200,200, text=("ruby", user_one), font=("Comic Sans", 20))
-        J_three = score_canvas.create_text(200,300, text=("java", user_one), font=("Comic Sans", 20))
-        J_four = score_canvas.create_text(200,400, text=("sqlite", user_one), font=("Comic Sans", 20))
-        J_five = score_canvas.create_text(200,500, text=("openCV", user_one), font=("Comic Sans", 20))
+        J_one = score_canvas.create_text(200,100, text=("python", user_one), font=("Comic Sans", 10))
+        J_two = score_canvas.create_text(200,200, text=("ruby", user_one), font=("Comic Sans", 10))
+        J_three = score_canvas.create_text(200,300, text=("java", user_one), font=("Comic Sans", 10))
+        J_four = score_canvas.create_text(200,400, text=("sqlite", user_one), font=("Comic Sans", 10))
+        J_five = score_canvas.create_text(200,500, text=("openCV", user_one), font=("Comic Sans", 10))
         bottom_frame = Frame(self,bg = "white")
         bottom_frame.pack()
         bottom_frame.place(relx = 0.2,rely = 0.9)
         start_button = Button(bottom_frame,width = 20, text = "Start", fg = "black",\
                               command = lambda:controller.show_frame(start_page))
-        exit_button = Button(bottom_frame,width = 20, text = "Close", fg = "black",\
+        exit_button = Button(bottom_frame,width = 20, text = "Exit", fg = "black",\
                              command = lambda:app.exit())
         start_button.pack(side = LEFT,padx = 5,pady = 5)
         exit_button.pack(side = RIGHT,padx = 30,pady = 5)
@@ -226,6 +224,6 @@ class score_page(Frame):
 if __name__ == "__main__":
     app = game_wrapper()
     app.resizable(0,0)
-    app.iconbitmap('C:\\Users\\abhayksi\\Desktop\\GUI_python\\favicon.ico')
+    app.iconbitmap('favicon.ico')
     background_label.image = background_image
     app.mainloop()
