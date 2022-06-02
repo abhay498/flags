0289778aa6feb70e74a22a9134c6002d7cff515e
Abhay Kumar Singh <abskumar798@gmail.com>
2022-06-02 15:04:21 +0530

    Updated flags.py
    Updated flags.py to restart the application on start and freezing buttons once a button is clicked to freeze score and result

--- flags.py
+++ flags.py
@@ -23,6 +23,8 @@ global flag
 global difficulty_level
 difficulty_level = 'country_hard'
 
+import sys
+import os
 
 def func_options(chosen):
     country_1 = ['Switzerland', 'Canada', 'Australia', 'Vietnam']
@@ -69,11 +71,21 @@ def change_flag(top_frame, bottom_frame, button1, button2, button3, button4, con
     button3["text"] = options_text[2]
     button4["text"] = options_text[3]
 
+    button1['state'] = NORMAL
+    button2['state'] = NORMAL
+    button3['state'] = NORMAL
+    button4['state'] = NORMAL
+    
     counter += 1
-
 
 def print_result(number, id_2, id_1, controller):
     global userscore_canvas, chosen, flag, score, difficulty_level
+    global button1, button2, button3, button4
+    button1['state'] = DISABLED
+    button2['state'] = DISABLED
+    button3['state'] = DISABLED
+    button4['state'] = DISABLED
+    
     if flag == number:
         userscore_canvas.itemconfigure(id_2, text="Right")
         if difficulty_level == 'country_easy':
@@ -105,24 +117,6 @@ def about():
             "Comic Sans",
             10))
 
-
-def scores():
-    window = Toplevel()
-    window.title('Scores')
-    window.geometry("400x400")
-    window.iconbitmap('icon.ico')
-    window.resizable(0, 0)
-    canvas = Canvas(window, width=400, height=400, bg='white')
-    canvas.pack()
-    J_one = canvas.create_text(
-        150,
-        150,
-        text=("Ram       40 \r\rShyam       60"),
-        font=(
-            "Comic Sans",
-            10))
-
-
 class game_wrapper(Tk):
     def __init__(self, *args, **kwargs):
         Tk.__init__(self, *args, **kwargs)
@@ -130,7 +124,8 @@ class game_wrapper(Tk):
         self.title("flags")
         self.frames = {}
         self.configure(bg="white")
-
+        self.name = ''
+        
         container = Frame(self)
         container.pack(side='top', fill='both', expand=True)
         container.grid_rowconfigure(0, weight=1)
@@ -153,11 +148,11 @@ class game_wrapper(Tk):
     def exit_window(self, window):
         window.destroy()
 
-
 class flags_page(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         global canvas, score, userscore_canvas, counter, country, shuffled_country
+        global button1, button2, button3, button4
         score = 0
         canvas = Canvas(self, width=300, height=150, bg='white')
         canvas.pack()
@@ -182,7 +177,7 @@ class flags_page(Frame):
             40, 30, text=(
                 "Score", score), font=(
                 "Comic Sans", 10))
-        id_2 = userscore_canvas.create_text(200, 30, font=("Comic Sans", 10))
+        id_2 = userscore_canvas.create_text(300, 30, font=("Comic Sans", 10))
 
         # Set the options here
         options = options_text = func_options('Australia')
@@ -230,7 +225,7 @@ class flags_page(Frame):
         button2.pack(side=RIGHT, padx=5, pady=5)
         button3.pack(side=LEFT, padx=5, pady=5)
         button4.pack(side=RIGHT, padx=5, pady=5)
-
+        
         button5 = Button(
             next_frame,
             width=20,
@@ -252,14 +247,9 @@ class flags_page(Frame):
         # File
         filemenu = Menu(menubar, tearoff=0)
         filemenu.add_command(label="Start",
-                             command=lambda: controller.show_frame(start_page))
+                             command=lambda: restart_program())
         filemenu.add_command(label="Exit", command=lambda: app.exit())
         menubar.add_cascade(label="File", menu=filemenu)
-
-        # Options
-        options = Menu(menubar, tearoff=0)
-        options.add_command(label="Scores", command=lambda: scores())
-        menubar.add_cascade(label="Options", menu=options)
 
         # Help
         helper = Menu(menubar, tearoff=0)
@@ -272,53 +262,47 @@ class flags_page(Frame):
 class start_page(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
-        global background_image
-        global background_label
-        global svalue
-
-        background_image = PhotoImage(file='white.png')
-        background_label = Label(self, image=background_image)
-        background_label.pack(side='top', fill='both', expand='yes')
-        label = Label(background_label, text="Name", font=LARGE_FONT)
-        label.pack(pady=200, padx=100)
-        play_button_font = font.Font(family='Verdana', size=10)
-        play_button = Button(
-            background_label,
-            width=30,
-            text="Play",
-            fg="black",
-            command=lambda: controller.show_frame(flags_page),
-            height=5,
-            font=play_button_font)
-        play_button.pack(padx=0, pady=0, expand=True)
-        play_button.pack()
+        
         self.configure(bg="white")
-        svalue = StringVar()
-        w = Entry(self, textvariable=svalue)
-        w.pack()
+        play_button_font = font.Font(family='Arial', size=10)
+        play_button = Button(
+            self,
+            width=50,
+            text='Play',
+            fg='black',
+            command=lambda: controller.show_frame(flags_page),
+            height=4,
+            font=play_button_font)
+        play_button.pack(side=BOTTOM, padx = 10, pady = 150)
+        
+        label = Label(self, text='Name', font=("Arial ", 15))
+        label.pack(side=LEFT, padx = 40, pady = 30)
 
+        name_variable = StringVar()
+        name_entry = Entry(self,width=30,textvariable = name_variable, font=('Arial 15'))
+        name_entry.pack(side=LEFT, padx = 10)
 
 class PlayAgainExit(Frame):
     def __init__(self, parent, controller):
         Frame.__init__(self, parent)
         self.configure(bg="white")
-        global counter
+        global counter, app
         score_canvas = Canvas(self, width=500, height=700, bg="white")
 
         if counter == 3:
             score_canvas.create_text(
                 300,
                 50,
-                text="Wrong answer",
+                text="Congratulations, you have \r identified all flags correctly.",
                 fill="black",
                 font=('Helvetica 15 bold'))
         else:
             score_canvas.create_text(
                 300,
                 50,
-                text="Congratulations, you have \r identified all flags correctly.",
+                text="Wrong answer",
                 fill="black",
-                font=('Helvetica 15 bold'))
+                font=('Arial 15 bold'))
             
         score_canvas.pack()
         bottom_frame = Frame(self, bg="white")
@@ -329,15 +313,21 @@ class PlayAgainExit(Frame):
             width=20,
             text="Play Again",
             fg="black",
-            command=lambda: controller.show_frame(start_page))
+            command=lambda: restart_program())
         exit_button = Button(bottom_frame, width=20, text="Exit", fg="black",
                              command=lambda: app.exit())
         start_button.pack(side=LEFT, padx=5, pady=5)
         exit_button.pack(side=RIGHT, padx=30, pady=5)
 
+def restart_program():
+    """Restarts the current program.
+    Note: this function does not return. Any cleanup action (like
+    saving data) must be done before calling this function."""
+    python = sys.executable
+    os.execl(python, python, * sys.argv)
+
 if __name__ == "__main__":
     app = game_wrapper()
     app.resizable(0, 0)
     app.iconbitmap('icon.ico')
-    background_label.image = background_image
     app.mainloop()
